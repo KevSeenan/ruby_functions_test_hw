46fec84e6a5caafd354be7d0d22942a40669b7e3
KevSeenan <kev.seenan@gmail.com>
2019-07-16 16:28:43 +0100

    commit for test_add_string_as_number

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -34,8 +34,14 @@
 #
 # end
 
-def join_string(string_1, string_2)
+# def join_string(string_1, string_2)
+#
+#   return string_1 + string_2
+#
+# end
 
-  return string_1 + string_2
+def add_string_as_number(string_1, string_2)
 
+  return string_1.to_i + string_2.to_i
+  
 end
